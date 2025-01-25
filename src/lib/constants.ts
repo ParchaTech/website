368aa3b46b1ba030634f1a9bea13f2fdf5ff1fc3
JoseSanclementePT <jose.sanclemente@parcha.tech>
2025-01-25 16:44:36 -0500

    Move assets to Contentful Changes: - Upload images and icons to Contentful

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,1 +1,8 @@
 export const LOGO_ID = "p1PDXPq27OqSSiRydaaS8";
+export const BACKGROUND_SPHERE_ID = "4oZWsVzlxv4flAGjb5mkN5";
+export const BACKGROUND_LEFT_GRID = "left grid";
+export const BACKGROUND_RIGHT_GRID = "right grid";
+export const TEAM_PHOTO_ID = "5yfaYIeKbP67skMuuE8xy5";
+export const ICONS_ID = "icon";
+export const PARCHATECH_CURVE_TEXT_ID = "6ykcp5g1RA3Kkxd1ualrjv";
+export const ARROW_UP_ICON_ID = "4lJF9SkkxoYJNO7m3uy7UX";
